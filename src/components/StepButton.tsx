bfd83a1d0834dce9a9968dbd876287def986bf14
scorbettUM <sean.corbett@umontana.edu>
2023-03-03 16:17:24 -0600

    sc-dev disabled dark-mode text fix

--- src/components/StepButton.tsx
+++ src/components/StepButton.tsx
@@ -30,7 +30,7 @@ export const StepButton = ({
         <div className="w-full flex justify-center items-center pb-4">
             <button 
                 type={buttonType === "SUBMIT" ? "submit" : "button"}
-                className={`text-4xl px-4 py-4 dark:text-white text-slate-900 flex items-center justify-center ${buttonState === "ENABLED" ? "" : "cursor-not-allowed text-[#13131a]/25 text-[#b9b9b9] dark:text-[#afafaf]/50 dark:bg-[#24252a]"}`}
+                className={`text-4xl px-4 py-4 dark:text-white text-slate-900 flex items-center justify-center ${buttonState === "ENABLED" ? "" : "cursor-not-allowed text-[#d3d3d3] dark:text-[#5a5a5a] dark:bg-[#24252a]"}`}
                 onClick={() => {
 
                     if (step !== FORM_STEPS.SUBMITTED){
